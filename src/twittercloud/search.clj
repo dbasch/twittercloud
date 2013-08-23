ecaac8d7832d532ad0d1ef6a563155763be34bb1
Diego Basch <dbasch@gmail.com>
2013-08-23 15:44:54 -0700

    cosmetic changes

--- src/twittercloud/search.clj
+++ src/twittercloud/search.clj
@@ -1,44 +1,46 @@
 (ns twittercloud.search
-  (:use [clojure.string :only (split trim lower-case)]
-        twittercloud.config twittercloud.util [twittercloud.core :only (draw)]
-        [twitter.oauth]
-        [twitter.api.search])
+  (:use [clojure.string :only (split trim lower-case)])
+  (:require twitter.api.search twitter.oauth twittercloud.core
+            [twittercloud.config :as conf]
+            [twittercloud.util :as util])
+   
   (:gen-class :main true))
 
 ;; twitter credentials
-(def creds (make-oauth-creds twitter-key
-                             twitter-secret
-                             oauth-access-token
-                             oauth-access-token-secret))
+(def creds (twitter.oauth/make-oauth-creds
+            conf/twitter-key
+            conf/twitter-secret
+            conf/oauth-access-token
+            conf/oauth-access-token-secret))
 
-(defn maxid[tweets]
+(defn oldest-id [tweets]
   (dec (reduce min (map :id tweets))))
 
 ;; search twitter for results older than max-id, concatenate the text into a string
 ;; if max-id is -1, then we get the most recent tweets
-(defn fetch-tweets[buf query total max-id]
-  (let [results (search :oauth-creds creds
-                        :params {:q query :count 100 :max_id max-id})
+(defn fetch-tweets [buf query total max-id]
+  (let [results (twitter.api.search/search
+                 :oauth-creds creds
+                 :params {:q query :count 100 :max_id max-id})
         tweets (:statuses (:body results))
         new-total (+ total (count tweets))
         new-buf (str (print-str (map :text tweets)) " " buf)]
     (println "tweets fetched: " new-total)
-    
+
+    ;; keep searching for older tweets until we have at least 1500
     (if (< new-total 1500)
-      ;; keep searching for older tweets
-      (recur new-buf query new-total (maxid tweets))
-      ;; we have enough tweets
+      (recur new-buf query new-total (oldest-id tweets))
       new-buf)))
+
+;; keep "interesting" words, remove the query word (it would dwarf everything else)
+(defn keeper[word query-word] (and (util/interesting word) (not= word query-word)))
 
 ;; draw a word cloud for the most recent tweets containing a given word.
 (defn -main [& args]
-
-  ;; filter out hashtags and the query word (it would dwarf everything else)
-  (defn keeper[word] (and (wanted word) (not= (first word) \#) (not= word (first args))))
-
   (let [filename (str (first args) ".png")
-        textbuffer (fetch-tweets " " (first args) 0 -1)
-        wordlist (filter keeper (status-tokens (lower-case textbuffer)))
+        query-word (first args)
+        textbuffer (fetch-tweets " " query-word 0 -1)
+        wordlist (filter #(keeper % query-word) (util/status-tokens (lower-case textbuffer)))
         words (apply str (interpose " " wordlist))]
-    (draw words filename))
+    (twittercloud.core/draw words filename))
     (System/exit 0))
--- src/twittercloud/util.clj
+++ src/twittercloud/util.clj
@@ -1,7 +1,7 @@
 (ns twittercloud.util
- ;; Utilities for parsing text
- (:gen-class :main true)
- (:require clojure.string))
+  ;; Utilities for parsing text
+  (:gen-class :main true)
+  (:require clojure.string))
 
 (def stopwords (set (clojure.string/split (slurp "stopwords.txt") #"\n")))
 
@@ -11,20 +11,20 @@
 (def mentions (partial re-seq #"@\S+"))
 
 (defn remove-urls[text]
-   (reduce #(.replace %1 %2 "") text (urls text)))
+  (reduce #(.replace %1 %2 "") text (urls text)))
 
 (defn remove-mentions[text]
-   (reduce #(.replace %1 %2 "") text (mentions text)))
+  (reduce #(.replace %1 %2 "") text (mentions text)))
 
 (defn tokenize[text]
- (clojure.string/split text #"[\"\s#&!:,;\.\\\+-]+"))
+  (clojure.string/split text #"[\"\s#&!:,;\.\\\+-]+"))
 
 (defn status-tokens[text] ((comp tokenize remove-urls remove-mentions) text))
 
 ;; heuristic for deciding which words to keep from a twitter-style corpus
-(defn wanted[word]
-   (if (and (not= (first word) \@)
-        (= nil (stopwords word))
-        (> (.length word) 2))
-    true false)
-)
+(defn interesting[word]
+  (if (and (not= (first word) \@)
+           (not= (first word) \#)
+           (= nil (stopwords word))
+           (> (.length word) 2))
+    true false))
