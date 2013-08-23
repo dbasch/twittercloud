bf9262d58acb39e2617f4b4477698903470cd076
Diego Basch <dbasch@gmail.com>
2013-08-22 20:42:31 -0700

    made fetch-tweets and main more readable

--- src/twittercloud/search.clj
+++ src/twittercloud/search.clj
@@ -12,18 +12,29 @@
 (defn maxid[tweets]
   (dec (reduce min (map :id tweets))))
 
+;; search twitter for results older than max-id, concatenate the text into a string
+;; if max-id is -1, then we get the most recent tweets
 (defn fetch-tweets[buf query total max-id]
-  (println total)
-  (let [results ; no max id the first time, after that we go further down into the timeline
-        (search :oauth-creds creds
-                :params {:q query :count 100 :max_id max-id})]
-    (let [tweets (:statuses (:body results))]
-      (if (< (+ total (count tweets)) 1500)
-        (recur (str (print-str (map :text tweets)) " " buf) query (+ total (count tweets)) (maxid tweets))
-         buf))))
+  (let [results (search :oauth-creds creds
+                        :params {:q query :count 100 :max_id max-id})]
+    (let [tweets (:statuses (:body results))
+          new-total (+ total (count tweets))]
+      (println "Tweets fetched: " new-total)
+      ;; have we collected at least 1500 tweets yet?
+      (if (< new-total 1500)
+        (let [newbuf (str (print-str (map :text tweets)) " " buf)]
+          ;; keep searching for older tweets
+          (recur newbuf query new-total (maxid tweets)))
+        buf))))
 
 (defn -main [& args]
-                                        ;don't show hashtags or the query word (it would be huge)
+
+  ;; filter out hashtags and the query word (it would dwarf everything else)
   (defn keeper[word] (and (wanted word) (not= (first word) \#) (not= word (first args))))
-  (draw (apply str (interpose " " (filter keeper (status-tokens (lower-case (fetch-tweets " " (first args) 0 -1)))))) (str (first args) ".png"))
-  (System/exit 0))
+
+  (let [filename (str (first args) ".png")
+        textbuffer (fetch-tweets " " (first args) 0 -1)
+        wordlist (filter keeper (status-tokens (lower-case textbuffer)))
+        words (apply str (interpose " " wordlist))]
+    (draw words filename))
+    (System/exit 0))
