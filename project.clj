545e9a1c9439936d4873ae94368b54972835c402
Diego Basch <dbasch@gmail.com>
2013-08-31 19:45:28 -0700

    removed unnecessary dependencies

--- project.clj
+++ project.clj
@@ -2,13 +2,9 @@
   :description "Create a word cloud from a twitter searchusing WordCram"
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-time "0.4.4"]
                  [wordcram "0.5.6"]
                  [cue "1.0.0"]
                  [jsoup "1.3.3"]
                  [processing "1.0.3"]
-                 [twitter-api "0.7.4"]
-                 [com.novemberain/monger "1.2.0"]
-                 [org.clojure/data.json "0.1.3"]
-                 [hiccup "1.0.1"]]
+                 [twitter-api "0.7.4"]]
   :main twittercloud.search)
